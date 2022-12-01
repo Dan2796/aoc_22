f8ae5331ec0f410705582636624b9c5038d4aaad
Dan Snow <danjsnow@outlook.com>
2022-12-01 11:45:41 +0000

    Added structure with abstract Day2022 class and also solutions to day 1

---
+++ src/main/solutions/days/Day1.java
@@ -0,0 +1,36 @@
+package main.solutions.days;
+import java.util.*;
+
+public class Day1 extends Day2022 {
+    Day1(){
+        super(true, 1);
+    }
+
+    HashSet<Integer> calorieNumbers = new HashSet<>();
+    public void parseInput() {
+        int calorieCounter = 0;
+        while (input.hasNext()) {
+            String calorie = input.nextLine();
+            if (!Objects.equals(calorie, "")) {
+                calorieCounter += Integer.parseInt(calorie);
+            } else {
+                calorieNumbers.add(calorieCounter);
+                calorieCounter = 0;
+            }
+        }
+        // add last number in list:
+        calorieNumbers.add(calorieCounter);
+    }
+
+    public String getSolutionPart1() {
+        return Integer.toString(Collections.max(calorieNumbers));
+    }
+    public String getSolutionPart2(){
+        int first = Collections.max(calorieNumbers);
+        calorieNumbers.remove(first);
+        int second = Collections.max(calorieNumbers);
+        calorieNumbers.remove(second);
+        int third = Collections.max(calorieNumbers);
+        return Integer.toString(first + second + third);
+    }
+}
---
+++ src/main/solutions/days/Day2022.java
@@ -0,0 +1,41 @@
+package main.solutions.days;
+import java.util.*;
+import java.io.*;
+
+public abstract class Day2022 {
+    public static void main(String[] args) {
+        printSolutions(new Day1());
+    }
+
+    Scanner input;
+    int day;
+    public Day2022(boolean actual, int day){
+        this.day = day;
+        File file;
+        if (actual) {
+            file = new File("inputs/actual/day_" + day + ".txt");
+        }
+        else {
+            file = new File("inputs/example/day_" + day + ".txt");
+        }
+        try {
+            this.input = new Scanner(file);
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException();
+        }
+    }
+    public int getDay() {
+        return this.day;
+    }
+
+    public static void printSolutions(Day2022 day) {
+        day.parseInput();
+        System.out.println("Day " + day.getDay() + " solution to part 1: " + day.getSolutionPart1());
+        System.out.println("Day " + day.getDay() + " solution to part 2: " + day.getSolutionPart2());
+    }
+    public abstract void parseInput();
+    public abstract String getSolutionPart1();
+    public abstract String getSolutionPart2();
+}
+
+
