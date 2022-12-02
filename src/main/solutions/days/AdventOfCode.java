14dd625e85519ca9d8b0670e6642f970221697e8
Dan Snow <danjsnow@outlook.com>
2022-12-02 10:04:31 +0000

    Added day 2

--- src/main/solutions/days/AdventOfCode.java
+++ src/main/solutions/days/AdventOfCode.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class AdventOfCode {
     public static void main(String[] args) {
-        main.solutions.days.Day.printSolutions(new Day1(true));
+        main.solutions.days.Day.printSolutions(new Day2(true));
     }
 }
 
