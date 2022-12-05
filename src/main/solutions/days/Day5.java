7e9dd1ee946424731d7714ffd8798e59f690fcbd
Dan Snow <danjsnow@outlook.com>
2022-12-05 19:27:36 +0000

    Improved readability of day 5, including separate Instructions class

--- src/main/solutions/days/Day5.java
+++ src/main/solutions/days/Day5.java
@@ -12,48 +12,40 @@ class Day5 extends Day{
     }
 
     ArrayList<Stack<Character>> stacksList = new ArrayList<>();
-    ArrayList<Integer[]> instructions = new ArrayList<>();
+    ArrayList<Instruction> instructions = new ArrayList<>();
     @Override
     void parseInput() {
         ArrayList<String> rawStartingStacks = new ArrayList<>();
-        int numberOfStacks = 0;
         // start by parsing the stack map
-        while(input.hasNext()) {
-            String row = input.nextLine();
-            if (row.equals("")) {
-                String[] stackNumbers = rawStartingStacks.get(rawStartingStacks.size() - 1).split(" ");
-                numberOfStacks = Integer.parseInt(stackNumbers[stackNumbers.length - 1]);
-                rawStartingStacks.remove(rawStartingStacks.size() - 1);
-                break;
-            }
+        String row = input.nextLine();
+        while(!row.equals("")) {
             rawStartingStacks.add(row);
+            row = input.nextLine();
         }
+        // while loop leaves row as the numbering of the stacks - can use to get stack number
+        String[] stackNumbers = rawStartingStacks.get(rawStartingStacks.size() - 1).split(" ");
+        int numberOfStacks = Integer.parseInt(stackNumbers[stackNumbers.length - 1]);
+        rawStartingStacks.remove(rawStartingStacks.size() - 1);
         // now go in reverse and start creating the stacks from the bottom up
         Collections.reverse(rawStartingStacks);
-        for (int i = 0; i <= numberOfStacks - 1; i++)
+        for (int i = 0; i <= numberOfStacks - 1; i++) {
             stacksList.add(new Stack<>());
-        for (String row: rawStartingStacks) {
+        }
+        for (String line: rawStartingStacks) {
             int stackNumber = 0;
-            for (int i = 1; i < row.length(); i += 4) {
-                if (row.charAt(i) != ' ') {
-                    stacksList.get(stackNumber).add(row.charAt(i));
+            for (int i = 1; i < line.length(); i += 4) {
+                if (line.charAt(i) != ' ') {
+                    stacksList.get(stackNumber).add(line.charAt(i));
                 }
                 stackNumber++;
             }
         }
         // now parse the instructions - note minus 1 for stack index since we index from zero
         while(input.hasNext()) {
-            String[] instructionLine = input.nextLine().split(" ");
-            instructions.add(new Integer[]{ Integer.parseInt(instructionLine[1]),
-                                            Integer.parseInt(instructionLine[3]) - 1,
-                                            Integer.parseInt(instructionLine[5]) - 1});
+            instructions.add(new Instruction(input.nextLine().split(" ")));
         }
     }
 
-    public static void execInstruction(ArrayList<Stack<Character>> stacksList, Integer[] instruction) {
-        for (int i = 0; i < instruction[0]; i++)
-            stacksList.get(instruction[2]).push(stacksList.get(instruction[1]).pop());
-    }
     @Override
     StringBuilder getSolutionPart1() {
         // make copy so can keep original for part 2, or could re-run this function
@@ -61,37 +53,57 @@ class Day5 extends Day{
         for (Stack<Character> stack: stacksList) {
             stacksListP1.add((Stack<Character>) stack.clone());
         }
-        for (Integer[] instruction: instructions) {
-            execInstruction(stacksListP1, instruction);
+        for (Instruction instruction: instructions) {
+            instruction.crateMover9000(stacksListP1);
         }
         StringBuilder topOfEach = new StringBuilder();
-        for (Stack<Character> stack: stacksListP1)
+        for (Stack<Character> stack: stacksListP1) {
             topOfEach.append(stack.peek());
+        }
         return topOfEach;
     }
 
-    public static void execInstructionP2(ArrayList<Stack<Character>> stacksList, Integer[] instruction) {
-        Stack<Character> temp = new Stack<>();
-        for (int i = 0; i < instruction[0]; i++)
-            temp.push(stacksList.get(instruction[1]).pop());
-        for (int i = 0; i < instruction[0]; i++)
-            stacksList.get(instruction[2]).push(temp.pop());
-    }
     @Override
     StringBuilder getSolutionPart2() {
         ArrayList<Stack<Character>> stacksListP2 = new ArrayList<>();
         for (Stack<Character> stack: stacksList) {
             stacksListP2.add((Stack<Character>) stack.clone());
         }
-        for (Integer[] instruction: instructions) {
-            execInstructionP2(stacksListP2, instruction);
+        for (Instruction instruction: instructions) {
+            instruction.crateMover9001(stacksListP2);
         }
         StringBuilder topOfEach = new StringBuilder();
-        for (Stack<Character> stack: stacksListP2)
+        for (Stack<Character> stack: stacksListP2) {
             topOfEach.append(stack.peek());
+        }
         return topOfEach;
 
     }
 }
 
+class Instruction {
+    final private int nToMove, moveTo, moveFrom;
 
+    Instruction(String[] instructionString) {
+        nToMove = Integer.parseInt(instructionString[1]);
+        // note subtracting one so that we can use them as indices starting at 0
+        moveFrom = Integer.parseInt(instructionString[3]) - 1;
+        moveTo = Integer.parseInt(instructionString[5]) - 1;
+    }
+
+    public void crateMover9000(ArrayList<Stack<Character>> stacksList) {
+        for (int i = 0; i < this.nToMove; i++) {
+            stacksList.get(this.moveTo).push(stacksList.get(this.moveFrom).pop());
+        }
+    }
+
+    public void crateMover9001(ArrayList<Stack<Character>> stacksList) {
+        Stack<Character> temp = new Stack<>();
+        for (int i = 0; i < this.nToMove; i++) {
+            temp.push(stacksList.get(this.moveFrom).pop());
+        }
+        for (int i = 0; i < this.nToMove; i++) {
+            stacksList.get(this.moveTo).push(temp.pop());
+        }
+    }
+}
