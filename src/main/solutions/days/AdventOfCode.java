bddda4984a241dc3a9b8e124b121cba406669aac
Dan Snow <danjsnow@outlook.com>
2022-12-28 14:42:48 +0000

    Day 11 part 1 completed

--- src/main/solutions/days/AdventOfCode.java
+++ src/main/solutions/days/AdventOfCode.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class AdventOfCode {
     public static void main(String[] args) throws NoSolutionFoundException, ParsingException {
-        main.solutions.days.Day.printSolutions(new Day10(true));
+        main.solutions.days.Day.printSolutions(new Day11(true));
     }
 }
 
